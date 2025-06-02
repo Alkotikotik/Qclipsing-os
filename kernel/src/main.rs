f477dbef6cd8b159bdcfe74ec5fe6f46c15d5d62
qclipsing <alkotikotik@gmail.com>
2025-06-02 17:34:11 +0200

    The first commit

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,9 +2,44 @@
 #![no_main] //No main for the same reason
 use core::panic::PanicInfo;// Panic is unmanagable error
 
-#[unsafe(no_mangle)] //Our own version of "main"
+static HELLO: &[u16] = b"
+
+ /$$   /$$           /$$ /$$                       
+| $$  | $$          | $$| $$                       
+| $$  | $$  /$$$$$$ | $$| $$  /$$$$$$              
+| $$$$$$$$ /$$__  $$| $$| $$ /$$__  $$             
+| $$__  $$| $$$$$$$$| $$| $$| $$  \ $$             
+| $$  | $$| $$_____/| $$| $$| $$  | $$             
+| $$  | $$|  $$$$$$$| $$| $$|  $$$$$$/             
+|__/  |__/ \_______/|__/|__/ \______/              
+                                                   
+                                                   
+                                                   
+ /$$      /$$                     /$$       /$$ /$$
+| $$  /$ | $$                    | $$      | $$| $$
+| $$ /$$$| $$  /$$$$$$   /$$$$$$ | $$  /$$$$$$$| $$
+| $$/$$ $$ $$ /$$__  $$ /$$__  $$| $$ /$$__  $$| $$
+| $$$$_  $$$$| $$  \ $$| $$  \__/| $$| $$  | $$|__/
+| $$$/ \  $$$| $$  | $$| $$      | $$| $$  | $$    
+| $$/   \  $$|  $$$$$$/| $$      | $$|  $$$$$$$ /$$
+|__/     \__/ \______/ |__/      |__/ \_______/|__/
+
+";
+
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! 
 {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() 
+    {
+        unsafe 
+        {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
